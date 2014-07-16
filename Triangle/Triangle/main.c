37852a3140871d92fa085d81cd147090fafafd2b
Omer Wazir <omer.wazir@gmail.com>
2014-07-16 08:35:16 -0700

    Added Triangle project

--- Triangle/Triangle/main.c
+++ Triangle/Triangle/main.c
@@ -8,8 +8,16 @@
 
 #include <stdio.h>
 
+float remainingAngle(float firstAngle,float secondAngle){
+    float thirdAngle = 180 - (firstAngle + secondAngle);
+    return thirdAngle;
+}
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    float angleA = 30.0;
+    float angleB = 60.0;
+    float angleC = remainingAngle(angleA, angleB);
+    printf("The third angle is %.2f.\n", angleC);
+    
     return 0;
 }
