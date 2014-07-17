95f24f7ed6b7d9a2dbe90cf8366b04c2b1a320ab
Omer Wazir <omer.wazir@gmail.com>
2014-07-17 08:37:45 -0700

    Doubtful it will rain. Today's high could be 98F

--- Addresses/Addresses/main.c
+++ Addresses/Addresses/main.c
@@ -7,9 +7,22 @@
 //
 
 #include <stdio.h>
+#include <limits.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    int i = 17;
+    int *addressOfi = &i;
+    printf("i stores it's value at %p\n", addressOfi);
+    printf("this function starts at %p\n", main);
+    printf("the int stored at addressOfi is %d\n", *addressOfi);
+     *addressOfi = 2014;
+    printf("the value of i is now %d\n", i);
+    printf("An int is %zu bytes\n", sizeof(int));
+    printf("A float is %zu bytes\n", sizeof(float));
+    printf("A short is %zu bytes\n", sizeof(short));
+    printf("The largest value a signed short can hold is %d\n", SHRT_MAX);
+    printf("The smallest value a signed short can hold is %d\n", SHRT_MIN);
+    printf("The largest value an unsigned short can hold is %d\n", USHRT_MAX);
+    printf("A pointer is %zu bytes\n", sizeof(int *));
     return 0;
 }
