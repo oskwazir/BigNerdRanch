8df5336d266810516f0f730b6d305497ded072a7
Omer Wazir <omer.wazir@gmail.com>
2014-07-28 19:03:38 -0700

    Gave employee assets to own. Will see how ARC deallocates assets when employeed doesn't own an asset anymore.

--- BMITime/BMITime/BNREmployee.h
+++ BMITime/BMITime/BNREmployee.h
@@ -8,13 +8,19 @@
 
 #import <Foundation/Foundation.h>
 #import "BNRPerson.h"
+@class BNRAsset;
 
-@interface BNREmployee : BNRPerson
+@interface BNREmployee : BNRPerson{
+    NSMutableArray *_assets;
+}
 
 @property (nonatomic) unsigned int employeedID;
 @property (nonatomic) unsigned int officeAlarmCode;
 @property (nonatomic) NSDate *hireDate;
+@property (nonatomic, copy) NSArray *assets;
 
 -(double) yearsOfEmployment;
+-(void) addAsset: (BNRAsset *)asset;
+-(unsigned int) valueOfAssets;
 
 @end
--- BMITime/BMITime/BNREmployee.m
+++ BMITime/BMITime/BNREmployee.m
@@ -7,8 +7,41 @@
 //
 
 #import "BNREmployee.h"
+#import "BNRAsset.h"
 
 @implementation BNREmployee
+
+// Accessors for assets properties
+
+//We only expose an immutable array but locally use a mutable array.
+-(void) setAssets:(NSArray *)assets{
+    _assets = [assets mutableCopy];
+}
+
+-(NSArray *) assets{
+    return [_assets copy];
+}
+
+-(void) addAsset:(BNRAsset *)asset{
+ //Is _assets nil?
+    if(!_assets){
+        //Create the array
+        _assets = [[NSMutableArray alloc] init];
+    }
+    
+    [_assets addObject:asset];
+}
+
+-(unsigned int)valueOfAssets{
+ //Sum up the resale value of the assets
+    unsigned int sum = 0;
+    for(BNRAsset *asset in _assets){
+        sum += [asset resaleValue];
+    }
+    
+    return sum;
+
+}
 
 -(float) bodyMassIndex{
     float normalBMI = [super bodyMassIndex];
@@ -29,7 +62,11 @@
 }
 
 -(NSString *) description{
-    return [NSString stringWithFormat:@"<Employeed %d>",self.employeedID];
+    return [NSString stringWithFormat:@"Employeed %u: $%u in assets.",[self employeedID],[self valueOfAssets]];
+}
+
+-(void) dealloc{
+    NSLog(@"deallocating %@", self);
 }
 
 @end
