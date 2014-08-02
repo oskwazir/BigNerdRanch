6396b9513da01b52da6bd01dc089d7d6d2f33259
Omer Wazir <omer.wazir@gmail.com>
2014-08-02 11:21:33 -0700

    Using a block to remove vowels from strings

---
+++ VowelMovement/VowelMovement/main.m
@@ -0,0 +1,49 @@
+//
+//  main.m
+//  VowelMovement
+//
+//  Created by Omer Wazir on 8/2/14.
+//  Copyright (c) 2014 Omer Wazir. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        //create an array of strings and one for devowelized ones
+        NSArray *originalStrings = @[@"Darth Vader",@"Luke Skywalker",@"Princess Leia",
+                                     @"Chewbacca",@"C3P0",@"R2-D2",@"Han Solo",@"Yoda",@"Obi Wan Kenobi"];
+        
+        NSLog(@"The original strings %@",originalStrings);
+        
+        NSMutableArray *devowelizedStrings = [[NSMutableArray alloc]init];
+        
+        //create a list of characters to be removed from the strings
+        NSArray *vowels = @[@"a",@"e",@"i",@"o",@"u"];
+        
+        //declare the block variable
+        void (^devowelizer)(id, NSUInteger, BOOL *);
+        
+        //compose a block and assign it to the variable
+        devowelizer = ^(id string, NSUInteger i, BOOL *stop){
+            NSMutableString *newString = [NSMutableString stringWithString:string];
+            
+            //iterate over the array of vowels, replacing occurrences of each vowel with an empty string
+            for(NSString *vowel in vowels){
+                NSRange fullRange = NSMakeRange(0, [newString length]);
+                [newString replaceOccurrencesOfString:vowel
+                                           withString:@""
+                                              options:NSCaseInsensitiveSearch
+                                                range:fullRange];
+            }
+            
+            [devowelizedStrings addObject:newString];
+        }; //this is the end of the block assignment and needs a semi-colon to terminate it
+        
+        
+        //Now lets iterate over the originalStrings array using our devowelizer block!
+        [originalStrings enumerateObjectsUsingBlock:devowelizer];
+        NSLog(@"devowelized strings: %@", devowelizedStrings);
+    }
+    return 0;
+}
